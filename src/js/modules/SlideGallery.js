1acab10c97948891fb5157c68caf5dc8a5dcd622
高岩ゆりえ <takaiwa@tsumikiinc.com>
2017-04-16 22:23:54 +0900

    [add]slide-galleryのnextボタンのfunction追加

--- src/js/modules/SlideGallery.js
+++ src/js/modules/SlideGallery.js
@@ -11,47 +11,61 @@ export default class SlideGallery {
   init() {
     this.buildGallery();
     this.buildNext();
-    this.buildPrev();
+    // this.buildPrev();
   }
   buildGallery() {
     let str = [];
+    let color = [];
     for (var i = 0; i < this.data.length; i++) {
       str[i] = `<div class="p-slide-gallery-item">${this.data[i].label}</div>`;
+    }
+    for (var i = 0; i < this.data.length; i++) {
+      color[i] = this.data[i].color;
     }
     let strLength = str.length - 1;
     this.$inner.html(str[strLength]);
     for (var i = 0; i < strLength; i++) {
       this.$inner.append(str[i]);
     }
+
   }
   buildNext() {
     let str = this.$inner.children();
     let strLength = str.length - 1;
       $('.p-slide-gallery-controler-button--next').on('click',function(){
-      $('.p-slide-gallery-inner').addClass('is-animate-next')
-      if($('.p-slide-gallery-inner').hasClass('is-animate-next')) {
-        $('.p-slide-gallery-inner').html(str[1]);
-        for (var i = 2; i < strLength + 1; i++) {
-          $('.p-slide-gallery-inner').append(str[i])
-        }
-        $('.p-slide-gallery-inner').append(str[0]);
-        // $('.p-slide-gallery-inner').removeClass('is-animate-next');
-      }
+        $('.p-slide-gallery-inner').animate({
+          'marginLeft':'-66.66666666%'
+        },function(){
+            $('.p-slide-gallery-inner').html(str[1]);
+            for (var i = 2; i < strLength + 1; i++) {
+              $('.p-slide-gallery-inner').append(str[i])
+            }
+            $('.p-slide-gallery-inner').append(str[0]);
+            $('.p-slide-gallery-inner').css('margin-left', '-33.33333333%');
+            str = $('.p-slide-gallery-inner').children();
+            strLength = str.length - 1;
+        });
     });
   }
 
 
 
-  // buildNextGallery() {
+  // buildPrev() {
   //   let str = this.$inner.children();
   //   let strLength = str.length - 1;
-  //
-  // }
-
-
-  // buildPrev() {
-  //   this.$prev.on('click',function(){
-  //     $('.p-slide-gallery-inner').addClass('is-animate-prev');
-  //     });
+  //     $('.p-slide-gallery-controler-button--next').on('click',function(){
+  //       $('.p-slide-gallery-inner').animate({
+  //         'marginLeft':'0'
+  //       },function(){
+  //           $('.p-slide-gallery-inner').html(str[1]);
+  //           for (var i = 2; i < strLength + 1; i++) {
+  //             $('.p-slide-gallery-inner').append(str[i])
+  //           }
+  //           $('.p-slide-gallery-inner').append(str[0]);
+  //           $('.p-slide-gallery-inner').css('margin-left', '-33.33333333%');
+  //           str = $('.p-slide-gallery-inner').children();
+  //           strLength = str.length - 1;
+  //       });
+  //   });
   // }
 }
